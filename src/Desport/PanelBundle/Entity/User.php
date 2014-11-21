d593db60bbf250d48881fa08602ff346003383ff
lexbar <zumbenet@gmail.com>
2014-11-21 17:57:45 +0100

    User creation and cache/logs deleted

---
+++ src/Desport/PanelBundle/Entity/User.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Desport\PanelBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="fos_user")
+ * @ORM\Entity
+ */
+class User extends BaseUser
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
