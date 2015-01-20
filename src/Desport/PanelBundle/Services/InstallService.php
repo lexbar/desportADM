18cdee8e0281d5f6d2df4179e5077d709b43f0a0
lexbar <zumbenet@gmail.com>
2015-01-20 23:04:16 +0100

    Error reporting on install service

--- src/Desport/PanelBundle/Services/InstallService.php
+++ src/Desport/PanelBundle/Services/InstallService.php
@@ -59,7 +59,7 @@ class InstallService
 	    }
 	    else //ERROR
 	    {
-    	    //die(print_r($result,1));
+    	    die(print_r($result,1));
 		    return false; //"ERROR generando el subdominio -- ".$result['text'].": ".$result['details'];
 	    } 
     }
@@ -153,6 +153,7 @@ class InstallService
 	    }
 	    else //ERROR
 	    {
+    	    die(print_r($result,1));
 		    return false; //"ERROR generando la base de datos -- ".$result['text'].": ".$result['details'];
 	    } 
     }
