7d6aa28b02e6317a2f8563b6dc4e78c9db3655e7
lexbar <zumbenet@gmail.com>
2015-09-12 15:10:23 +0200

    Normalize characters

--- web/js/script.js
+++ web/js/script.js
@@ -30,8 +30,30 @@ function loadStep2() {
 }
 
 function fillSignupUrl(el) {
-    $('#signup_url').val( $(el).val().replace(/[^a-zA-Z]/g,'').toLowerCase() );
+    $('#signup_url').val( normalize($(el).val()).replace(/[^a-zA-Z]/g,'').replace(/[^a-zA-Z]/g,'').toLowerCase() );
 }
+
+var normalize = (function() {
+  var from = "ÃÀÁÄÂÈÉËÊÌÍÏÎÒÓÖÔÙÚÜÛãàáäâèéëêìíïîòóöôùúüûÑñÇç", 
+      to   = "AAAAAEEEEIIIIOOOOUUUUaaaaaeeeeiiiioooouuuunncc",
+      mapping = {};
+ 
+  for(var i = 0, j = from.length; i < j; i++ )
+      mapping[ from.charAt( i ) ] = to.charAt( i );
+ 
+  return function( str ) {
+      var ret = [];
+      for( var i = 0, j = str.length; i < j; i++ ) {
+          var c = str.charAt( i );
+          if( mapping.hasOwnProperty( str.charAt( i ) ) )
+              ret.push( mapping[ c ] );
+          else
+              ret.push( c );
+      }      
+      return ret.join( '' );
+  }
+ 
+})();
 
 function submitStep2() {
     $('#step2').addClass('animated fadeOutUp');
